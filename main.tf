b5b1e50da8d2e554f0e24520db60898a0de6e8ee
santygcp <97255596+santygcp@users.noreply.github.com>
2022-12-29 04:48:45 +0530

    4

--- main.tf
+++ main.tf
@@ -1,13 +1,42 @@
-resource "google_storage_bucket" "bucket" {
-  name     = "test-bucket-random-001122-22"
-  location = "europe-west2"
+resource "google_storage_bucket" "static-site" {
+  name          = "image-store.com"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
 }
 
-resource "google_storage_bucket" "gcs_bucket" {
-  name     = "test-bucket-random-00112323"
-  location = "europe-west2"
-}
+resource "google_storage_bucket" "auto-expire" {
+  name          = "auto-expiring-bucket"
+  location      = "US"
+  force_destroy = true
 
-resource "google_storage_bucket" "gcs_bucket1ef" {
-  name = "test-bucket-random-00112853883"
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
