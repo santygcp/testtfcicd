c6a5799ebf95d505455ddc00d1fad605be29127b
santygcp <97255596+santygcp@users.noreply.github.com>
2022-12-29 03:09:18 +0530

    add

--- main.tf
+++ main.tf
@@ -7,3 +7,7 @@ resource "google_storage_bucket" "gcs_bucket" {
   name     = "test-bucket-random-00112323"
   location = "europe-west2"
 }
+
+resource "google_storage_bucket" "gcs_bucket" {
+  name = "test-bucket-random-001123"
+}
