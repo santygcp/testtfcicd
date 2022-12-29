d8b1af716aef3395f322b42b7577fd623872f8e1
santygcp <97255596+santygcp@users.noreply.github.com>
2022-12-29 12:05:52 +0530

    finaaal

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "auto-expire" {
-  name          = "auto-expiring-bucket"
+resource "google_storage_bucket" "randimjnfjjf" {
+  name          = "akbf-test-bucket"
   location      = "US"
   force_destroy = true
 }
